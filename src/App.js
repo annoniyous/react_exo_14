63e4214861c2d4106f8ce519a953a95c744b1a36
annoniyous <bouysantiman.y@outlook.com>
2021-01-20 11:47:54 +0100

    Exo fini

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
+import Hooks from './components/Hooks'
 import './App.css';
-import React from 'react'
+import React,{useState} from 'react'
 
-export default function App() {
-  const [varia, setVaria] = useState("Hello world")
-  return (
-    <div>
+
+
+function App() {
+
+const [varia, setVaria] = useState("Hello world")
+const [autrevaria, setAutrevaria] = useState("Hello city")
+return (
+  <div>
       <h1>{varia}</h1>
+      <Hooks planet = {autrevaria}/>
     </div>
   )
 }
 
+export default App
---
+++ src/components/Hooks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hooks.css'
+
+ function Hooks(propos) {
+    return (
+        <div>
+            <h2>{propos.planet}</h2>
+        </div>
+    )
+}
+
+export default Hooks
